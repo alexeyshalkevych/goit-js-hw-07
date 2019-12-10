68046fb10c104e209b5c38b0d8f5abff01e86f6e
Alexey <valnistuy@gmail.com>
2019-12-10 23:49:53 +0200

    added task-06

---
+++ js/task-06.js
@@ -0,0 +1,27 @@
+"use strict";
+
+// Напиши скрипт, который бы при потере фокуса на инпуте, проверял его содержимое
+// на правильное количество символов.
+
+// Сколько символов должно быть в инпуте, указывается в его атрибуте data-length.
+// Если введено подходящее количество, то border инпута становится зеленым,
+// если неправильное - красным.
+
+const inputField = document.querySelector("#validation-input");
+
+const hendlerInputValidation = e => {
+  const valueDataLengthAttribute = Number(
+    e.currentTarget.getAttribute("data-length")
+  );
+  const valueInputLength = e.currentTarget.value.length;
+
+  if (valueDataLengthAttribute === valueInputLength) {
+    inputField.classList.add("valid");
+    inputField.classList.remove("invalid");
+  } else {
+    inputField.classList.remove("valid");
+    inputField.classList.add("invalid");
+  }
+};
+
+inputField.addEventListener("blur", hendlerInputValidation);
