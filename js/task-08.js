923441f92b562d1cc1df337febed515e8bb9c736
Alexey <valnistuy@gmail.com>
2019-12-11 01:37:28 +0200

    added task-08

---
+++ js/task-08.js
@@ -0,0 +1,56 @@
+"use strict";
+
+// Напиши скрипт создания и очистки коллекции элементов. Пользователь вводит количество
+// элементов в input и нажимает кнопку Создать, после чего рендерится коллекция.
+// При нажатии на кнопку Очистить, коллекция элементов очищается.
+
+// Создай функцию createBoxes(amount), которая принимает 1 параметр amount - число.
+// Функция создает столько div, сколько указано в amount и добавляет их в div#boxes.
+
+// Каждый созданный div:
+
+// Имеет случайный rgb цвет фона
+// Размеры самого первого div - 30px на 30px
+// Каждый следующий div после первого, должен быть шире и выше предыдущего на 10px
+// Создай функцию destroyBoxes(), которая очищает div#boxes.
+
+const itemBoxes = document.querySelector("#boxes");
+const controlInput = document.querySelector("#controls > input");
+
+const renderButton = document.querySelector('button[data-action="render"]');
+const destroyButton = document.querySelector('button[data-action="destroy"]');
+
+const createBoxes = amount => {
+  let size = 30;
+
+  for (let i = 1; i <= amount; i++) {
+    const item = document.createElement("div");
+
+    item.style.backgroundColor = `rgb(${Math.floor(
+      Math.random() * 256
+    )},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)})`;
+
+    if (i === 1) {
+      item.style.width = `${size}px`;
+      item.style.height = `${size}px`;
+    }
+
+    size += 10;
+
+    item.style.width = `${size}px`;
+    item.style.height = `${size}px`;
+
+    itemBoxes.appendChild(item);
+  }
+};
+
+const destroyBoxes = () => {
+  itemBoxes.innerHTML = "";
+};
+
+renderButton.addEventListener("click", () => {
+  createBoxes(controlInput.value);
+  controlInput.value = "";
+});
+
+destroyButton.addEventListener("click", destroyBoxes);
